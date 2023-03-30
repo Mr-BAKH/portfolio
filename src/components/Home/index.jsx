c24b2996ae37b080f3330c1408987721eec0cbd0
Mr-BAKH <mr.bakh.usb@gmail.com>
2023-03-31 03:21:29 +0430

    edit title of the page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -23,10 +23,10 @@ const color =  token;
                 color: color.gray[100],
             }}   
         >
-                <span style={{fontSize:"22px",color:color.blue[200],textAlign:"center",fontFamily: `Titan One', cursive`,fontWeight:'bolder'}}>web & logo Designer</span>
-                <div className='rightHeader'>
-                    <Brightness4Icon className='icon'/>
-                </div>
+            <span style={{fontSize:"22px",color:color.blue[200],textAlign:"center",fontFamily: `Titan One', cursive`,fontWeight:'bolder'}}>WEB & LOGO<span style={{fontSize:"14px"}}>_ Designig Group</span></span>
+            <div className='rightHeader'>
+                <Brightness4Icon className='icon'/>
+            </div>
            </div>
            
             {/* my profile and desctiption of me */}
@@ -41,7 +41,7 @@ const color =  token;
                     <h1 style={{color: color.blue[800]}}>Hi,there</h1>
                     <h3 style={{color: color.blue[700]}}>wellcome to my portfolio.</h3>
                     <p style={{margin:'20px 0px',color: color.gray[900],fontSize: '18px'}}>
-                    Our team can provide you with various services in the field of website design. You can visit our team and the work we have done so far.
+                    Our team can provide you with various services in the field of Web & Logo designingn. You can visit our team and the work we have done so far.
                     </p>
                 </div>
                 <img className='imgHellow' src={localimage} alt="amirIage"/>
